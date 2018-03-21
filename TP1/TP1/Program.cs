237ab53d6db2fa1cb7469dad92713b6776297d38
TomilloCasir <36413008+TomilloCasir@users.noreply.github.com>
2018-03-21 17:59:59 -0300

    Add files via upload

---
+++ TP1/TP1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Realizar un programa de consola que permita ingresar el nombre y apellido por separado, con estos datos debe imprimir un mensaje con el nombre completo.
+
+            //Console.WriteLine("Ingresar nombre:");
+            //string nombre = Console.ReadLine();
+            //Console.WriteLine("Ingresar apellido:");
+            //string apellido = Console.ReadLine();
+
+            //Console.WriteLine(nombre + " " + apellido);
+            //Console.Read();
+
+            //Realizar un programa de consola que permita ingresar el nombre y la edad, con estos datos debe imprimir un mensaje diciendo si es mayor de edad o no.
+
+            //Console.WriteLine("Ingresar nombre:");
+            //string nombre = Console.ReadLine();
+            //Console.WriteLine("Ingresar edad:");
+            //int edad = Convert.ToInt16(Console.ReadLine());
+
+            //Console.WriteLine(edad >= 18 ? "Es mayor" : "Es menor");
+            //Console.Read();
+
+            //Realizar un programa de consola que permita ingresar un numero y calcule la suma de todos los numeros enteros anteriores
+
+            //Console.WriteLine("Ingresar numero:");
+            //int numero = Convert.ToInt16(Console.ReadLine());
+            //int sumaTotal = 0;
+
+            //for (int i = 0; i < numero; i++)
+            //{
+            //    sumaTotal += i;
+            //}
+
+            //Console.WriteLine("La suma total de los numeros anteriores es: " + sumaTotal);
+            //Console.Read();
+
+            //Realizar un programa de consola que permita ingresar 2 valores y encuentr el maximo comun divisor
+
+            //Console.WriteLine("Ingresar numero 1:");
+            //int num1 = Convert.ToInt16(Console.ReadLine());
+            //Console.WriteLine("Ingresar numero 2:");
+            //int num2 = Convert.ToInt16(Console.ReadLine());
+            //int divisor = num1 > num2 ? num2 : num1;
+
+            //while (num1 % divisor != 0 || num2 % divisor != 0)
+            //{
+            //    divisor--;
+            //}
+
+            //Console.WriteLine("El maximo comun divisor es: " + divisor);
+            //Console.Read();
+
+            //Realizar un programa de consola que permita ingresar numeros y que termine de pedir numeros cuando se ingresa 0 y calcule el promedio de todos los ingresados
+
+            //Console.WriteLine("Ingresar numero:");
+            //int numero = Convert.ToInt16(Console.ReadLine());
+            //int contador = 0;
+            //int sumaTotal = 0;
+            //int promedio = 0;
+
+            //while (numero != 0)
+            //{
+            //    sumaTotal += numero;
+            //    contador++;
+            //    Console.WriteLine("Ingresar numero");
+            //    numero = Convert.ToInt16(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Promedio total: " + (promedio = sumaTotal / contador));
+            //Console.Read();
+
+        }
+    }
+}
